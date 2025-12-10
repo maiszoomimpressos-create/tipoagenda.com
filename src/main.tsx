d4ab3072ea60acd85d391ce55d6c1f731d8fd86a
[dyad] <edricolpani@gmail.com>
2025-12-10 07:39:11 -0300

    [dyad] Depurando a perda do hash da URL de redefinição de senha - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+// Adicionado para depuração inicial da URL
+console.log('main.tsx - window.location.href:', window.location.href);
+console.log('main.tsx - window.location.hash:', window.location.hash);
+
 createRoot(document.getElementById("root")!).render(<App />);
