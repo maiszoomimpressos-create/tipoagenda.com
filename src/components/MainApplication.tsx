5099dbf09857b768fd633270c0cdb842e1971940
[dyad] <edricolpani@gmail.com>
2025-12-10 22:22:18 -0300

    [dyad] Tentativa de correção de erro de parsing JSX - wrote 1 file(s)

--- src/components/MainApplication.tsx
+++ src/components/MainApplication.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
